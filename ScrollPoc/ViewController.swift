f5a1d759e81cf602f715f2c86019eb910af41eb7
Magno Augusto Ferreira Ruivo <magnoaugustofr@gmail.com>
2020-03-04 21:42:16 -0300

    first commit

---
+++ ScrollPoc/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  ScrollPoc
+//
+//  Created by Magno Augusto Ferreira Ruivo on 04/03/20.
+//  Copyright © 2020 Magno Augusto Ferreira Ruivo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var scroll: UIScrollView = UIScrollView()
+    private var content: UIView = UIView()
+    private var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.backgroundColor = .cyan
+        return stack
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollSetup()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scroll.contentSize = CGSize(width: stackView.frame.width, height: stackView.frame.height)
+    }
+
+    func scrollSetup() {
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scroll)
+        NSLayoutConstraint.activate([
+            scroll.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0),
+            scroll.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            scroll.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0),
+            scroll.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+        
+//        content.frame.size.height = 48
+//        content.backgroundColor = .purple
+//        content.translatesAutoresizingMaskIntoConstraints = false
+//        scroll.addSubview(content)
+//        NSLayoutConstraint.activate([
+//            content.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0),
+//            content.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+//            content.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0),
+//            content.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+//            content.widthAnchor.constraint(equalTo: scroll.widthAnchor)
+//        ])
+        
+        scroll.addSubview(stackView)
+        stackView.spacing = 10
+        NSLayoutConstraint.activate([
+            stackView.leftAnchor.constraint(equalTo: scroll.leftAnchor, constant: 0),
+            stackView.topAnchor.constraint(equalTo: scroll.topAnchor, constant: 0),
+            stackView.rightAnchor.constraint(equalTo: scroll.rightAnchor, constant: 0),
+            stackView.bottomAnchor.constraint(equalTo: scroll.bottomAnchor, constant: 0),
+            stackView.widthAnchor.constraint(equalTo: scroll.widthAnchor)
+        ])
+        
+        for _ in 1 ..< 100 {
+            let label1 = UILabel()
+            label1.text = "potatoe"
+            NSLayoutConstraint.activate([
+                label1.widthAnchor.constraint(equalToConstant: 150)
+            ])
+            let label2 = UILabel()
+            label2.text = "💪🥔"
+            
+            let stack = UIStackView()
+            stack.axis = .horizontal
+            stack.distribution = .fillEqually
+            stack.addArrangedSubview(label1)
+            stack.addArrangedSubview(label2)
+            
+            stackView.addArrangedSubview(stack)
+//            let vw = UIButton(type: UIButton.ButtonType.system)
+//            vw.setTitle("Button", for: .normal)
+//            stackView.addArrangedSubview(vw)
+        }
+    }
+
+}
+
